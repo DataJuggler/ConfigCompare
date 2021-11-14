bfb466b0e3aa12467b211ae746072147a5d85f38
Corby <chichols@rcp.com>
2021-11-14 09:16:16 -0600

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace ConfigCompare
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SourceControl = new DataJuggler.Win.Controls.LabelTextBoxBrowserControl();
+            this.TargetControl = new DataJuggler.Win.Controls.LabelTextBoxBrowserControl();
+            this.CompareButton = new DataJuggler.Win.Controls.Button();
+            this.ResultsListBox = new System.Windows.Forms.ListBox();
+            this.CopyButton = new DataJuggler.Win.Controls.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SourceControl
+            // 
+            this.SourceControl.BackColor = System.Drawing.Color.Transparent;
+            this.SourceControl.BrowseType = DataJuggler.Win.Controls.Enumerations.BrowseTypeEnum.File;
+            this.SourceControl.ButtonColor = System.Drawing.Color.LemonChiffon;
+            this.SourceControl.ButtonImage = ((System.Drawing.Image)(resources.GetObject("SourceControl.ButtonImage")));
+            this.SourceControl.ButtonWidth = 48;
+            this.SourceControl.DarkMode = false;
+            this.SourceControl.DisabledLabelColor = System.Drawing.Color.Empty;
+            this.SourceControl.Editable = true;
+            this.SourceControl.EnabledLabelColor = System.Drawing.Color.Empty;
+            this.SourceControl.Filter = null;
+            this.SourceControl.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SourceControl.HideBrowseButton = false;
+            this.SourceControl.LabelBottomMargin = 0;
+            this.SourceControl.LabelColor = System.Drawing.Color.LemonChiffon;
+            this.SourceControl.LabelFont = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.SourceControl.LabelText = "Source:";
+            this.SourceControl.LabelTopMargin = 0;
+            this.SourceControl.LabelWidth = 80;
+            this.SourceControl.Location = new System.Drawing.Point(24, 16);
+            this.SourceControl.Name = "SourceControl";
+            this.SourceControl.OnTextChangedListener = null;
+            this.SourceControl.OpenCallback = null;
+            this.SourceControl.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.SourceControl.SelectedPath = null;
+            this.SourceControl.Size = new System.Drawing.Size(746, 32);
+            this.SourceControl.StartPath = null;
+            this.SourceControl.TabIndex = 0;
+            this.SourceControl.TextBoxBottomMargin = 0;
+            this.SourceControl.TextBoxDisabledColor = System.Drawing.Color.Empty;
+            this.SourceControl.TextBoxEditableColor = System.Drawing.Color.Empty;
+            this.SourceControl.TextBoxFont = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SourceControl.TextBoxTopMargin = 0;
+            this.SourceControl.Theme = DataJuggler.Win.Controls.Enumerations.ThemeEnum.Dark;
+            // 
+            // TargetControl
+            // 
+            this.TargetControl.BackColor = System.Drawing.Color.Transparent;
+            this.TargetControl.BrowseType = DataJuggler.Win.Controls.Enumerations.BrowseTypeEnum.File;
+            this.TargetControl.ButtonColor = System.Drawing.Color.LemonChiffon;
+            this.TargetControl.ButtonImage = ((System.Drawing.Image)(resources.GetObject("TargetControl.ButtonImage")));
+            this.TargetControl.ButtonWidth = 48;
+            this.TargetControl.DarkMode = false;
+            this.TargetControl.DisabledLabelColor = System.Drawing.Color.Empty;
+            this.TargetControl.Editable = true;
+            this.TargetControl.EnabledLabelColor = System.Drawing.Color.Empty;
+            this.TargetControl.Filter = null;
+            this.TargetControl.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TargetControl.HideBrowseButton = false;
+            this.TargetControl.LabelBottomMargin = 0;
+            this.TargetControl.LabelColor = System.Drawing.Color.LemonChiffon;
+            this.TargetControl.LabelFont = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TargetControl.LabelText = "Target:";
+            this.TargetControl.LabelTopMargin = 0;
+            this.TargetControl.LabelWidth = 80;
+            this.TargetControl.Location = new System.Drawing.Point(24, 80);
+            this.TargetControl.Name = "TargetControl";
+            this.TargetControl.OnTextChangedListener = null;
+            this.TargetControl.OpenCallback = null;
+            this.TargetControl.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TargetControl.SelectedPath = null;
+            this.TargetControl.Size = new System.Drawing.Size(746, 32);
+            this.TargetControl.StartPath = null;
+            this.TargetControl.TabIndex = 1;
+            this.TargetControl.TextBoxBottomMargin = 0;
+            this.TargetControl.TextBoxDisabledColor = System.Drawing.Color.Empty;
+            this.TargetControl.TextBoxEditableColor = System.Drawing.Color.Empty;
+            this.TargetControl.TextBoxFont = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TargetControl.TextBoxTopMargin = 0;
+            this.TargetControl.Theme = DataJuggler.Win.Controls.Enumerations.ThemeEnum.Dark;
+            // 
+            // CompareButton
+            // 
+            this.CompareButton.BackColor = System.Drawing.Color.Transparent;
+            this.CompareButton.ButtonText = "Compare";
+            this.CompareButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CompareButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CompareButton.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.CompareButton.Location = new System.Drawing.Point(626, 131);
+            this.CompareButton.Margin = new System.Windows.Forms.Padding(4);
+            this.CompareButton.Name = "CompareButton";
+            this.CompareButton.Size = new System.Drawing.Size(144, 48);
+            this.CompareButton.TabIndex = 2;
+            this.CompareButton.Theme = DataJuggler.Win.Controls.Enumerations.ThemeEnum.Dark;
+            this.CompareButton.Click += new System.EventHandler(this.CompareButton_Click);
+            // 
+            // ResultsListBox
+            // 
+            this.ResultsListBox.FormattingEnabled = true;
+            this.ResultsListBox.ItemHeight = 18;
+            this.ResultsListBox.Location = new System.Drawing.Point(24, 211);
+            this.ResultsListBox.Name = "ResultsListBox";
+            this.ResultsListBox.Size = new System.Drawing.Size(746, 292);
+            this.ResultsListBox.TabIndex = 3;
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.BackColor = System.Drawing.Color.Transparent;
+            this.CopyButton.ButtonText = "Copy";
+            this.CopyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CopyButton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CopyButton.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.CopyButton.Location = new System.Drawing.Point(626, 540);
+            this.CopyButton.Margin = new System.Windows.Forms.Padding(4);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(144, 48);
+            this.CopyButton.TabIndex = 4;
+            this.CopyButton.Theme = DataJuggler.Win.Controls.Enumerations.ThemeEnum.Dark;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.BackColor = System.Drawing.Color.Transparent;
+            this.StatusLabel.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.StatusLabel.Location = new System.Drawing.Point(24, 512);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(499, 34);
+            this.StatusLabel.TabIndex = 5;
+            this.StatusLabel.Text = "Ready.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::ConfigCompare.Properties.Resources.BlackImage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 630);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.ResultsListBox);
+            this.Controls.Add(this.CompareButton);
+            this.Controls.Add(this.TargetControl);
+            this.Controls.Add(this.SourceControl);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Config Compare";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataJuggler.Win.Controls.LabelTextBoxBrowserControl SourceControl;
+        private DataJuggler.Win.Controls.LabelTextBoxBrowserControl TargetControl;
+        private DataJuggler.Win.Controls.Button CompareButton;
+        private System.Windows.Forms.ListBox ResultsListBox;
+        private DataJuggler.Win.Controls.Button CopyButton;
+        private System.Windows.Forms.Label StatusLabel;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,259 @@
+﻿
+
+#region using statements
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataJuggler.Win.Controls;
+using DataJuggler.UltimateHelper;
+using XmlMirror.Runtime5.Objects;
+using System.IO;
+using DataJuggler.UltimateHelper.Objects;
+
+#endregion
+
+namespace ConfigCompare
+{
+
+    #region class MainForm
+    /// <summary>
+    ///  This is the main form for this app.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        
+        #region Private Variables
+        private List<FieldValuePair> sourceValues;
+        private List<FieldValuePair> targetValues;
+        #endregion
+        
+        #region Constructor
+        /// <summary>
+        /// Create a new instance of a 'MainForm' object.
+        /// </summary>
+        public MainForm()
+        {
+            // Create Controls
+            InitializeComponent();
+
+            // If you have config files you need to check often, you can set the values here.
+            // I removed this for check in, but if I ever leave my own values in by accident, forgive me.
+            // SourceControl.Text = @"";
+            // TargetControl.Text = @"";
+        }
+        #endregion
+
+        #region Events
+            
+            #region CompareButton_Click(object sender, EventArgs e)
+            /// <summary>
+            /// event is fired when the 'CompareButton' is clicked.
+            /// </summary>
+            private void CompareButton_Click(object sender, EventArgs e)
+            {
+                // clear in case it is run again
+                ResultsListBox.Items.Clear();
+
+                // Get the source values
+                SourceValues = GetValues(SourceControl.Text);
+
+                // Get the target values
+                TargetValues = GetValues(TargetControl.Text);
+
+                // verify both lists exist, and have at least one item
+                if (ListHelper.HasOneOrMoreItems(sourceValues, targetValues))
+                {
+                    // Iterate the collection of FieldValuePair objects
+                    foreach (FieldValuePair fieldValuePair in sourceValues)
+                    {
+                        // attempt to find this object in the target
+                        FieldValuePair target = targetValues.FirstOrDefault(x => x.FieldName == fieldValuePair.FieldName);
+
+                        // If the target object exists
+                        if (NullHelper.Exists(target))
+                        {
+                            // verify both objects exist, they should
+                            if (NullHelper.Exists(fieldValuePair.FieldValue, target.FieldValue))
+                            {
+                                // if the two values are not equal
+                                if (!TextHelper.IsEqual(fieldValuePair.FieldValue.ToString(), target.FieldValue.ToString()))
+                                {
+                                    // we have a difference                                    
+                                    ResultsListBox.Items.Add("App Setting " + fieldValuePair.FieldName + " has a different value in the target list.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // show a message
+                            ResultsListBox.Items.Add("App Setting " + fieldValuePair.FieldName + " was not found in the target list.");
+                        }
+                    }
+                }
+                else
+                {
+                    // Something went wrong, show a vague message to start with.
+                    MessageBox.Show("There is prfoblem loading one of the web.config files", "Configuration Not Valid");
+                }
+
+                // if there are differences
+                if (ResultsListBox.Items.Count > 0)
+                {
+                    // show the results
+                    StatusLabel.Text = "Found " + ResultsListBox.Items.Count + " differences.";
+                }
+                else
+                {
+                    // show the results
+                    StatusLabel.Text = "No differences found.";
+                }
+            }
+            #endregion
+            
+        #endregion
+
+        #region Methods
+            
+            #region GetValues(string filePath)
+            /// <summary>
+            /// returns a list of Values
+            /// </summary>
+            public List<FieldValuePair> GetValues(string filePath)
+            {
+                // initial value
+                List<FieldValuePair> values = new List<FieldValuePair>();
+
+                // locals
+                FieldValuePair fieldValuePair = null;
+                char[] delimiter = new char[] { ' ' };
+                char[] delimiter2 = new char[] { '=' };
+                string temp = "";
+                string temp2 = "";
+                string name = "";
+                string value = "";
+
+                // if the filePath exists
+                if (FileHelper.Exists(filePath))
+                {
+                    // get all the text
+                    string fileText = File.ReadAllText(filePath);
+
+                    // If the fileText string exists
+                    if (TextHelper.Exists(fileText))
+                    {
+                        // Get the textLines
+                        List<TextLine> textLines = TextHelper.GetTextLines(fileText);
+
+                        // If the textLines collection exists and has one or more items
+                        if (ListHelper.HasOneOrMoreItems(textLines))
+                        {
+                            // Iterate the collection of TextLine objects
+                            foreach (TextLine textLine in textLines)
+                            {
+                                // reset
+                                name = "";
+                                value = "";
+
+                                // if this is a line with a value
+                                if (textLine.Text.Trim().StartsWith("<add key="))
+                                {
+                                    // get the words, parsing on space only
+                                    List<Word> words = TextHelper.GetWords(textLine.Text, delimiter);
+
+                                    // if the words collection exists
+                                    if (ListHelper.HasXOrMoreItems(words, 3))
+                                    {
+                                        // Get the 2nd and 3rd words
+                                        temp = words[1].Text;
+                                        temp2 = words[2].Text;
+
+                                        // get the source words
+                                        words = TextHelper.GetWords(temp, delimiter2);
+
+                                        // if there are two words
+                                        if (ListHelper.HasXOrMoreItems(words, 2))
+                                        {
+                                            // Set the name
+                                            name = words[1].Text.Replace("\"", "");
+                                        }
+
+                                        // get the source words
+                                        words = TextHelper.GetWords(temp2, delimiter2);
+
+                                        // if there are two words
+                                        if (ListHelper.HasXOrMoreItems(words, 2))
+                                        {
+                                            // Set the name
+                                            value = words[1].Text.Replace("\"", "");
+                                        }
+                                    }
+
+                                    // If the strings name and value both exist
+                                    if (TextHelper.Exists(name, value))
+                                    {
+                                        // Create a new instance of a 'FieldValuePair' object.
+                                        fieldValuePair = new FieldValuePair();
+
+                                        // store the values
+                                        fieldValuePair.FieldName = name;
+                                        fieldValuePair.FieldValue = value;
+
+                                        // Add this value
+                                        values.Add(fieldValuePair);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // sort the list here
+                if (ListHelper.HasOneOrMoreItems(values))
+                {
+                    // sort the values by name
+                    values = values.OrderBy(x => x.FieldName).ToList();
+                }
+                
+                // return value
+                return values;
+            }
+            #endregion
+            
+        #endregion
+
+        #region Properties
+
+            #region SourceValues
+            /// <summary>
+            /// This property gets or sets the value for 'SourceValues'.
+            /// </summary>
+            public List<FieldValuePair> SourceValues
+            {
+                get { return sourceValues; }
+                set { sourceValues = value; }
+            }
+            #endregion
+            
+            #region TargetValues
+            /// <summary>
+            /// This property gets or sets the value for 'TargetValues'.
+            /// </summary>
+            public List<FieldValuePair> TargetValues
+            {
+                get { return targetValues; }
+                set { targetValues = value; }
+            }
+            #endregion
+            
+        #endregion
+    }
+    #endregion
+
+}
